bda60deb5849240c45445bdadd11efd45d9231cb
Boyan Krastev <bkrastev@web-teh.net>
2011-08-15 18:25:48 +0300

    Country select element, Unwired_Form base class

---
+++ library/Unwired/Form.php
@@ -0,0 +1,27 @@
+<?php
+
+class Unwired_Form extends Zend_Form
+{
+
+	public function init()
+	{
+		parent::init();
+
+		$this->addPrefixPath('Unwired_Form_', 'Unwired/Form');
+
+		$this->setDecorators(array('FormElements', 'Form'));
+
+		$this->setElementDecorators(array(
+										'element' => array('decorator' => 'ViewHelper'),
+							        	'label' => array('decorator' => 'Label',
+							            				 'options' => array('optionalSuffix' => ':',
+							                								'requiredSuffix' => ' * :',
+																			'placement' => 'prepend')
+														),
+										'errors' => 'errors',
+										'htmltag' => array('decorator' => 'HtmlTag',
+							            				   'options' => array ('tag' => 'div',
+																			   'class' => 'formelement'))
+										));
+	}
+}
