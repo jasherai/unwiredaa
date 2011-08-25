5233523b2065d4a279b7e7aa2783c5e3f6b8b904
Boyan Krastev <bkrastev@web-teh.net>
2011-08-25 13:12:05 +0300

    Tree relation model and viewhelper

---
+++ library/Unwired/Model/Tree.php
@@ -0,0 +1,104 @@
+<?php
+
+abstract class Unwired_Model_Tree extends Unwired_Model_Generic implements RecursiveIterator
+{
+	protected $_children = array();
+
+	protected $_parent = null;
+
+	private $_currentIdx = 0;
+
+	abstract public function getTreeBranchId();
+	abstract public function getTreeBranchName();
+
+	/* (non-PHPdoc)
+	 * @see RecursiveIterator::hasChildren()
+	 */
+	public function hasChildren() {
+		return (bool) count($this->_children) > 0;
+	}
+
+	/* (non-PHPdoc)
+	 * @see RecursiveIterator::getChildren()
+	 */
+	public function getChildren() {
+		return $this->current();
+	}
+
+	public function setChildren(array $children)
+	{
+		$this->_children = $children;
+		$this->rewind();
+
+		return $this;
+	}
+
+	public function addChild(Unwired_Model_Tree $child) {
+		if (!in_array($child, $this->_children)) {
+			$this->_children[] = $child;
+		}
+
+		return $this;
+	}
+
+	public function getParent()
+	{
+		return $this->_parent;
+	}
+
+	public function setParent(Unwired_Model_Tree $parent = null)
+	{
+		$this->_parent = $parent;
+		return $this;
+	}
+
+	/* (non-PHPdoc)
+	 * @see RecursiveIterator::current()
+	 */
+	public function current() {
+		if ($this->valid()) {
+			return $this->_children[$this->_currentIdx];
+		}
+
+		return null;
+	}
+
+	/* (non-PHPdoc)
+	 * @see RecursiveIterator::next()
+	 */
+	public function next() {
+		$this->_currentIdx++;
+
+		return $this->current();
+	}
+
+	/* (non-PHPdoc)
+	 * @see RecursiveIterator::key()
+	 */
+	public function key() {
+		if ($this->valid()) {
+			return $this->_currentIdx;
+		}
+		return null;
+	}
+
+	/* (non-PHPdoc)
+	 * @see RecursiveIterator::valid()
+	 */
+	public function valid() {
+		if (!$this->hasChildren() || $this->_currentIdx >= count($this->_children)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/* (non-PHPdoc)
+	 * @see RecursiveIterator::rewind()
+	 */
+	public function rewind() {
+		$this->_currentIdx = 0;
+	}
+
+
+}
---
+++ library/Unwired/View/Helper/Tree.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Tree helper - Generate UL from Unwired_Model_Tree for use with jsTree
+ *
+ * @uses viewHelper Unwired_View_Helper
+ */
+class Unwired_View_Helper_Tree {
+
+	/**
+	 * @var Zend_View_Interface
+	 */
+	public $view;
+
+	protected $_tree = null;
+
+	protected $_exclude = null;
+
+	protected $_options = array('prefix' => 'node_',
+								'class'	 => 'tree');
+	/**
+	 *
+	 */
+	public function tree(Unwired_Model_Tree $tree = null, Unwired_Model_Tree $exclude = null, array $options = array())
+	{
+		$this->_options = array_merge($this->_options, $options);
+		$this->_tree = $tree;
+		$this->_exclude = $exclude;
+		return $this;
+	}
+
+	public function render(Unwired_Model_Tree $tree = null)
+	{
+		if (null === $tree) {
+			if ($this->_tree == null) {
+				return '';
+			}
+
+			$tree = $this->_tree;
+		}
+
+		$result = "<ul class=\"{$this->getOption('class')}\">";
+		$result .= '<li id="group_' . $tree->getTreeBranchId() . '"><a href="javascript:;">'
+				. $tree->getTreeBranchName() . '</a>' . $this->_renderChildren($tree) . '</li>';
+		$result .= '</ul>';
+
+		return $result;
+	}
+
+	protected function _renderChildren(Unwired_Model_Tree $node)
+	{
+		if (!$node->hasChildren()) {
+			return '';
+		}
+
+		$result = '<ul>';
+		foreach ($node as $child) {
+			if ($this->_exclude && $child->getTreeBranchId() == $this->_exclude->getTreeBranchId()) {
+				continue;
+			}
+			$result .= '<li id="' . $this->getOption('prefix') . $child->getTreeBranchId() . '"><a href="javascript:;">'
+					. $child->getTreeBranchName() . '</a>' . $this->_renderChildren($child) . '</li>';
+		}
+
+		$result .= '</ul>';
+
+		return $result;
+	}
+
+	public function getOption($key)
+	{
+		if (!isset($this->_options[$key])) {
+			return null;
+		}
+
+		return $this->_options[$key];
+	}
+
+	public function __toString()
+	{
+		return $this->render();
+	}
+
+	/**
+	 * Sets the view field
+	 * @param $view Zend_View_Interface
+	 */
+	public function setView(Zend_View_Interface $view) {
+		$this->view = $view;
+	}
+}
