adc575e44cca9360350a6d4c00d777aeb7f2e80c
Boyan Krastev <bkrastev@web-teh.net>
2011-11-22 18:06:05 +0200

    Fixed missing parameters in findgroup

--- application/modules/groups/services/Group.php
+++ application/modules/groups/services/Group.php
@@ -5,17 +5,17 @@
 * Author & Copyright (c) 2011 Unwired Networks GmbH
 * alexander.szlezak@unwired.at
 *
-* Licensed under the terms of the Affero Gnu Public License version 3 
-* (AGPLv3 - http://www.gnu.org/licenses/agpl.html) or our proprietory 
+* Licensed under the terms of the Affero Gnu Public License version 3
+* (AGPLv3 - http://www.gnu.org/licenses/agpl.html) or our proprietory
 * license available at http://www.unwired.at/license.html
-*/  
+*/
 
 class Groups_Service_Group extends Unwired_Service_Tree
 {
 
 	public function findGroup($groupId, $parents = false, $children = false)
 	{
-		return parent::findNode($groupId);
+		return parent::findNode($groupId, $parents, $children);
 	}
 
 	/**
