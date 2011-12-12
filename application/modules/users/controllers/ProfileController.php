211a5c60c1f9f990999986ab27d2fd121503c1e1
Boyan Krastev <bkrastev@web-teh.net>
2011-12-12 18:11:07 +0200

    Allow users to edit their profile if they do not belong to group that has edit access.

--- application/modules/users/controllers/ProfileController.php
+++ application/modules/users/controllers/ProfileController.php
@@ -5,10 +5,10 @@
 * Author & Copyright (c) 2011 Unwired Networks GmbH
 * alexander.szlezak@unwired.at
 *
-* Licensed under the terms of the Affero Gnu Public License version 3 
-* (AGPLv3 - http://www.gnu.org/licenses/agpl.html) or our proprietory 
+* Licensed under the terms of the Affero Gnu Public License version 3
+* (AGPLv3 - http://www.gnu.org/licenses/agpl.html) or our proprietory
 * license available at http://www.unwired.at/license.html
-*/  
+*/
 
 class Users_ProfileController extends Unwired_Controller_Crud
 {
@@ -24,6 +24,7 @@ class Users_ProfileController extends Unwired_Controller_Crud
 
 	public function indexAction()
 	{
+	    $this->getAcl()->allow($this->_currentUser, $this->_currentUser, 'edit');
 		$this->_add(null, $this->_currentUser, new Users_Form_Profile());
 	}
 
