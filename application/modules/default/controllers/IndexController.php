d398e7c8a3cb9439544df46adc49daed5234329c
Boyan Krastev <bkrastev@web-teh.net>
2012-01-04 15:37:41 +0200

    Set stats interval to 3 seconds

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -76,7 +76,7 @@ class Default_IndexController extends Unwired_Controller_Action
             if (empty($stats)) {
                 $stats = $serviceChilli->getDeviceStatistics($location);
 
-                $this->_getCache()->save($stats, 'device_map_stats_' . $location, array('node', $location), 5);
+                $this->_getCache()->save($stats, 'device_map_stats_' . $location, array('node', $location), 3);
             }
         } else {
             $stats = $this->_getCache()->load('device_map_stats_total');
@@ -84,7 +84,7 @@ class Default_IndexController extends Unwired_Controller_Action
             if (empty($stats)) {
                 $stats = $serviceChilli->getInterfaceStatistics($interface);
 
-                $this->_getCache()->save($stats, 'device_map_stats_total', array('node'), 7);
+                $this->_getCache()->save($stats, 'device_map_stats_total', array('node'), 3);
             }
         }
 
