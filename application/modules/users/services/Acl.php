1a9910f4b6db584f72e11d9d002daa14fd61686f
Boyan Krastev <bkrastev@web-teh.net>
2012-01-05 14:17:53 +0200

    UNWIRED-27: Added check for admin groups when adding/editing network user

--- application/modules/users/services/Acl.php
+++ application/modules/users/services/Acl.php
@@ -5,10 +5,10 @@
 * Author & Copyright (c) 2011 Unwired Networks GmbH
 * alexander.szlezak@unwired.at
 *
-* Licensed under the terms of the Affero Gnu Public License version 3 
-* (AGPLv3 - http://www.gnu.org/licenses/agpl.html) or our proprietory 
+* Licensed under the terms of the Affero Gnu Public License version 3
+* (AGPLv3 - http://www.gnu.org/licenses/agpl.html) or our proprietory
 * license available at http://www.unwired.at/license.html
-*/  
+*/
 
 class Users_Service_Acl implements Zend_Acl_Assert_Interface
 {
@@ -55,8 +55,11 @@ class Users_Service_Acl implements Zend_Acl_Assert_Interface
 				/**
 				 * Checked category is child of current administrator's category
 				 */
-				if (!$acl->inherits($prefix . $checkGroupId, $prefix . $groupId)) {
-					continue;
+				if (!$resource instanceof Users_Model_NetUser || $groupId != $checkGroupId) {
+
+				    if (!$acl->inherits($prefix . $checkGroupId, $prefix . $groupId)) {
+					    continue;
+				    }
 				}
 
 				$result = $acl->isAllowed($prefix . $groupId, $resource, $privilege);
